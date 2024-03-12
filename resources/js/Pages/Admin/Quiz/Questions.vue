e357ff2468df54b2d68b0b2e086705d97de07f6b
BS956 <jahidul@brainstation-23.com>
2024-03-13 01:01:06 +0600

    Remove unnacceary filter option from quizzes

--- resources/js/Pages/Admin/Quiz/Questions.vue
+++ resources/js/Pages/Admin/Quiz/Questions.vue
@@ -43,30 +43,7 @@
                                     <InputText type="text" id="skill" v-model="skillFilter"
                                                placeholder="Enter Skill" aria-describedby="skill-help"/>
                                 </div>
-                                <div class="w-full flex flex-col mt-6">
-                                    <label for="topic" class="mb-3 text-sm font-semibold text-gray-800">{{ __('Topic') }}</label>
-                                    <InputText type="text" id="topic" v-model="topicFilter"
-                                               placeholder="Enter Topic" aria-describedby="topic-help"/>
-                                </div>
-                                <div class="w-full flex flex-col mt-6">
-                                    <label for="tag" class="pb-2 text-sm font-semibold text-gray-800">By Tag</label>
-                                    <v-select multiple id="tag" v-model="tagArray" :options="tags" label="name"
-                                              @search="searchTags" :dir="$page.props.rtl ? 'rtl' : 'ltr'">
-                                        <template v-slot:no-options="{ search, searching }">
-                                            <template v-if="searching">{{ __('No results were found for this search') }}.</template>
-                                            <em v-else class="opacity-50">{{ __('Start typing to search') }}.</em>
-                                        </template>
-                                    </v-select>
-                                </div>
-                                <div class="w-full flex flex-col mt-6">
-                                    <label class="mb-3 text-sm font-semibold text-gray-800">{{ __('Difficulty Level') }}</label>
-                                    <div class="flex flex-col gap-2">
-                                        <div class="p-field-radiobutton" v-for="difficulty in difficultyLevels">
-                                            <Checkbox :id="'difficulty'+difficulty.id" name="difficulty" :value="difficulty.id" v-model="difficultyFilter" />
-                                            <label class="text-sm text-gray-800" :for="'difficulty'+difficulty.id">{{ difficulty.name }}</label>
-                                        </div>
-                                    </div>
-                                </div>
+
                                 <div class="w-full flex items-center gap-2 my-8">
                                     <button type="button" @click="resetFilters" class="w-full qt-btn qt-btn-primary">{{ __('Reset') }}</button>
                                     <button type="button" @click="filterQuestions" class="w-full qt-btn qt-btn-success">{{ __('Search') }}</button>
